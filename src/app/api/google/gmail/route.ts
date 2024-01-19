82ff1bdf8162d4d811364dbaaaf2baff18df81ed
Shaharyar <Shaharyar321321@gmail.com>
2024-01-19 15:54:05 +0500

    Remove unused GET route in Gmail API

--- src/app/api/google/gmail/route.ts
+++ src/app/api/google/gmail/route.ts
@@ -36,30 +36,7 @@ export async function DELETE(req: NextRequest) {
     success: true,
   });
 }
-export async function GET(req: Request, res: Response) {
-  const client = await GoogleClient();
-  const { getAllEmails } = await handleGmail(client);
 
-  const generator = getAllEmails();
-  const encoder = new TextEncoder();
-  const stream = new ReadableStream({
-    async start(controller) {
-      for await (const email of generator) {
-        const chunk = encoder.encode(JSON.stringify(email) + "\n\n");
-        controller.enqueue(chunk);
-      }
-      controller.close();
-    },
-  });
-
-  return new Response(stream, {
-    headers: {
-      "Content-Type": "text/event-stream",
-      Connection: "keep-alive",
-      "Cache-Control": "no-cache, no-transform",
-    },
-  });
-}
 export async function POST(req: Request) {
   const client = await GoogleClient();
   const { getEmail } = await handleGmail(client);
